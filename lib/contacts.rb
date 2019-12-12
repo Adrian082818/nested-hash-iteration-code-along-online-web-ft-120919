24aa2f153c169b6c6eb1dd3c0351d80c84489104
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-12 14:12:51 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,12 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+contacts.each do |person, contact_detail_hash|
+  if person == "Freddy Mercury"
+    contact_detail_hash.each do |atrribute, data|
+      if atrribute == :favorite_ice_cream_flavors
+        data.delete_if{|ice_cream| ice_cream == "strawberry"}
+      end 
+    end 
+end 
 end
